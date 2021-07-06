33a41b3773b764cb1a46bdd2a5832aad804fa4ba
Nugus53 <julesnuguet53@hotmail.fr>
2021-07-06 11:00:27 +0200

    Ajout systéme de question

--- app.py
+++ app.py
@@ -1,16 +1,36 @@
-from flask import Flask, render_template, request
+from os import pardir
+from typing import Text
+from flask import Flask, render_template, request, jsonify
 from flask_socketio import SocketIO
+from src.Main.Model.Question import question,questionText
+import json
 
+#|- SETTING -|
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'tmpKey'
 socketio = SocketIO(app)
 clients = []
+t=0
 
 @socketio.event
 def connect():
     print('connection established')
     print(request.sid)
     clients.append(request.namespace)
+
+@app.route('/test')
+def QuestionTemplate():
+    return render_template('testQuestion.html')
+
+@socketio.on('question')
+def questionEvent(methods=['GET', 'POST']):
+    listQ=[questionText.questionText('Qui Mange des Pomme','Chirac').get_json(),questionText.questionText('Qui Mange des Pomme2','Chirac').get_json()]
+    socketio.emit('my question',listQ[1], callback=messageReceived)
+    #socketio.emit('my question',question.question(1,'test').get_json(), callback=messageReceived)
+
+@socketio.on('reponse')
+def reponseEvent(json, methods=['GET', 'POST']):
+    print(json)
 
 @app.route('/')
 def hello_world():
---
+++ src/Main/Model/Question/questionClassement.py
@@ -0,0 +1,6 @@
+import question
+class questionClassement(question):
+    def __init__(self, classable, question):
+       self.classable=classable;
+       self.question=question
+        
